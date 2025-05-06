931f8d3a925d0d77129de9df18b0702a9e02cdb7
LeonardoDiazz <20233tn141@utez.edu.mx>
2025-05-05 21:51:21 -0600

    Agrege mi edad

--- SayHello.java
+++ SayHello.java
@@ -2,5 +2,6 @@ public class SayHello {
     public static void main (String [] args){
         System.out.println ("Hola");
         System.out.println ("Leonardo");
+        System.out.println ("19");
     }
 }
